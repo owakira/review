c88afb7d196123ac13fd24bc80d7ad68fdab7154
Damir Mirasov <damirmirasovmain@gmail.com>
2023-05-08 21:13:19 +0600

    Create fail test

---
+++ src/test/java/com/github/owakira/review/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.owakira.review;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests {
+    @Test
+    public void test_Something() {
+        Assertions.assertEquals(4, 2 + 3);
+    }
+}
