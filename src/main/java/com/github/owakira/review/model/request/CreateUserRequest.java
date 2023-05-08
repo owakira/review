cf4695610e0ab64f45c2a353041f98ab9285982e
Damir Mirasov <72997396+owakira@users.noreply.github.com>
2023-05-09 03:30:54 +0600

    Create user endpoint validation (#4)
    * Add spring validation dependency
    
    * Add validation annotation for CreateUserRequest
    
    * Add valid annotation

--- src/main/java/com/github/owakira/review/model/request/CreateUserRequest.java
+++ src/main/java/com/github/owakira/review/model/request/CreateUserRequest.java
@@ -4,12 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Data
 @AllArgsConstructor
 public class CreateUserRequest {
+    @NotNull(message = "Email is required")
+    @Email(message = "Incorrect email")
     @ToString.Exclude
     private String email;
+
+    @NotNull(message = "Username is required")
+    @Pattern(
+            regexp = "^(?=.{3,32}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$",
+            message = "Incorrect username"
+    )
     private String username;
+
+    @NotNull(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "Incorrect password. Minimum eight characters, at least one letter and one number"
+    )
     @ToString.Exclude
     private String password;
 }
